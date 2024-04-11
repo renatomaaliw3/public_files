aa8a83b876f177119f0873b17e63efe6eb2c4bd4
renatomaaliw3 <renatomaaliw3@yahoo.com>
2024-04-11 19:25:25 +0800

    Save

--- Online Technologies/12. JavaScript & jQuery/00. jQuery Basics/js/main.js
+++ Online Technologies/12. JavaScript & jQuery/00. jQuery Basics/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  //console.log('Hello')
+  console.log('Hello');
 
   /* hide and show */
 
@@ -8,16 +8,16 @@ $(document).ready(function() {
 
   /* HTML and Text Manipulation (html & text), indexing eq, first, last */
 
-  $('#container > p:eq(1)').html('Replaced')
+  $('#container > p:eq(1)').html('<h1>Replaced</h1>')
 
   /* You can add CSS properties */
 
-  $('#container > p').css({'font-size': '18px', 'font-weight': 'bold'});
+  $('#container > p').css({'font-size': '18px', 'font-weight': 'bold', 'text-align': 'center'});
   $('#container > p:eq(1)').addClass('red');
 
   /* Attribute Extraction */
 
-  console.log($('#container img').attr('class'));
+  //console.log($('#container img').attr('class'));
   $('#container img').attr('alt', 'Man and Woman');
   $('#container img').attr('src', 'img/portfolio-02.jpg')
 
