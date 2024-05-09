267a768c514c95d8e0e9122b8f5cc2f8efd4bf80
renatomaaliw3 <renatomaaliw3@yahoo.com>
2024-05-09 12:56:55 +0800

    Save

--- Online Technologies/13. Backend (Pure PHP)/01. simple_crud/employeedb.sql
+++ Online Technologies/13. Backend (Pure PHP)/01. simple_crud/employeedb.sql
@@ -9,3 +9,26 @@ CREATE TABLE tblemployees (
 	PRIMARY KEY (employeeid)
 
 )
+
+-- CREATE (INSERT) --
+
+INSERT INTO tblemployees (lastname, firstname, department) VALUES ('Maaliw', 'Renato III', 'CIT')
+
+-- READ --
+
+SELECT * FROM tblemployees;
+
+-- READ WITH FILTER --
+
+SELECT * FROM tblemployees WHERE employeeid = 1
+
+-- UPDATE --
+
+UPDATE tblemployees SET lastname = 'Maaliw', firstname = 'Renato Jr.', department = 'CEN'
+WHERE employeeid = 1
+
+-- DELETE --
+
+DELETE FROM tblemployees WHERE employeeid = 1
+
+
